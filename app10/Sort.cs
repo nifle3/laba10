7bdaf4c8747c0ad37298fb875585658c89e786c6
nifie <kuper116rus.2005@gmail.com>
2023-03-25 20:15:20 +0300

    add sortclass

---
+++ app10/Sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app10
+{
+    interface ISortable
+    {
+        public int[] Sort(int[] array);
+    }
+
+    internal class BubbleSort : ISortable
+    {
+        public int[] Sort(int[] array)
+        {
+            return array;
+        }
+    }
+
+    internal class BitwiseSort : ISortable
+    {
+        public int[] Sort(int[] array)
+        {
+            return array;
+        }
+    }
+}
