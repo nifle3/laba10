5d7bb9c9aebfb4690e5fdac54b656fce1387a8ad
nifie <kuper116rus.2005@gmail.com>
2023-03-27 01:17:31 +0300

    add randomizer

---
+++ app10/Randomizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace app10
+{
+    internal class Randomizer
+    {
+        public static int[] GetRandomNumber(int quantity, int dozenCount = 1)
+        {
+            Random rnd = new Random();
+            List<int> digits = new List<int>();
+            
+            int lowest = (int)Math.Pow(10, dozenCount);
+            int highest = (int)Math.Pow(10, dozenCount+1);
+
+            for (int i = 0; i < quantity; i++)
+                digits.Add(rnd.Next(lowest, highest));
+
+            return digits.ToArray();
+        }
+    }
+}
