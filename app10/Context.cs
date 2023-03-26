9bc70bdb32b86594aab75012b38539d833351121
nifie <kuper116rus.2005@gmail.com>
2023-03-27 00:57:50 +0300

    add context class

---
+++ app10/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app10
+{
+    internal class Context
+    {
+        private ISortable? _sort;
+        public static int[]? array;
+
+        public Context(ISortable sort) => _sort = sort;
+        
+        public Context() { }
+
+        public void ExecutableAlgorithm() 
+        { 
+            if (array != null && _sort != null) 
+                _sort.Sort(array);
+        }
+    }
+}
